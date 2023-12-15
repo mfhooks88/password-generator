bd9226eb27da8e0222b4ac8fbbe840ec27e65e8e
mfhooks88 <mfhooks88@gmail.com>
2023-12-14 22:05:08 -0500

    added code to JS file

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # password-generator
 
-* pulled files from starter code
+## User Story
+
+* AS AN employee with access to sensitive data
+* I WANT to randomly generate a password that meets certain criteria
+* SO THAT I can create a strong password that provides greater security
+
+## Acceptance Criteria
+
+* GIVEN I need a new, secure password
+* WHEN I click the button to generate a password
+* THEN I am presented with a series of prompts for password criteria
+* WHEN prompted for password criteria
+* THEN I select which criteria to include in the password
+* WHEN prompted for the length of the password
+* THEN I choose a length of at least 8 characters and no more than 128 characters
+* WHEN asked for character types to include in the password
+* THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+* WHEN I answer each prompt
+* THEN my input should be validated and at least one character type should be selected
+* WHEN all prompts are answered
+* THEN a password is generated that matches the selected criteria
+* WHEN the password is generated
+* THEN the password is either displayed in an alert or written to the page
+
+## no changes made to the HTML or CSS files as these were already given
+
+## code was used from previous lessons from Bootcamp, and help found on MDN website and W3 schools website
--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+
+// added arrays for all characters that can be included in final password
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numeric = ["1","2","3","4","5","6","7","8","9","0"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")"]
+
+// created empty array for all combined characters to be chosen from
+var combinedCharacters = [];
+
+// created empty array for final password resulting from user choices
+var finalResult = []
+
+// added prompts and confirm commands for user
+var initialClick = prompt("please choose between 8 and 128 characters");
+var uppersIncluded = confirm("Uppercase letters included?");
+var lowersIncluded = confirm("Lowercase letters included?");
+var numbersIncluded = confirm("Numbers included?");
+var specialsIncluded = confirm("Special Characters included?");
+
+// creating if statements for each confirm chosen to be included in final array
+if (uppersIncluded) {
+  combinedCharacters = combinedCharacters.concat(upperCase);
+}
+
+if (lowersIncluded) {
+  combinedCharacters = combinedCharacters.concat(lowerCase);
+}
+
+if (numbersIncluded) {
+  combinedCharacters = combinedCharacters.concat(numeric);
+}
+
+if (specialsIncluded) {
+  combinedCharacters = combinedCharacters.concat(specialCharacters);
+}
+
+for (var i = 0; i <= initialClick; i++) {
+  finalResult.push(combinedCharacters[Math.random]);
+}
+
+return finalResult;
+}
+
+//Write password to the #password input - already given in starter code
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Add event listener to generate button - already given in starter code
 generateBtn.addEventListener("click", writePassword);
